f37a8f7333401052fc22e4fed2fa55c22f612d9b
tttom3669 <tttom3669@gmail.com>
2023-08-06 14:36:49 +0800

    update:banner

--- vite.config.js
+++ vite.config.js
@@ -25,7 +25,7 @@ function moveOutputPlugin() {
 export default defineConfig({
   // base 的寫法：
   // base: '/Repository 的名稱/'
-  base: '/2023_web-layout_week3/',
+  base: '/2023_web-layout_week3-4/',
   plugins: [
     liveReload(['./layout/**/*.ejs', './pages/**/*.ejs', './pages/**/*.html']),
     ViteEjsPlugin(),
